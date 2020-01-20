79d66b9a56e23fc65c5f48e79968871658eed81c
johnyuy <57468127+johnyuy@users.noreply.github.com>
2020-01-20 15:05:09 +0800

    Added Entity Framework

--- SSIMS/Models/Person.cs
+++ SSIMS/Models/Person.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 
 namespace SSIMS.Models
 {
     public class Person
     {
+        
         string name;
         string phoneNumber;
         string faxNumber;
