4393282b194da6ae91268ff2f8ba15c3b731d87c
johnyuy <57468127+johnyuy@users.noreply.github.com>
2020-01-20 15:02:14 +0800

    Added Person class in Models

---
+++ SSIMS/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SSIMS.Models
+{
+    public class Person
+    {
+        string name;
+        string phoneNumber;
+        string faxNumber;
+        string email;
+    }
+}
