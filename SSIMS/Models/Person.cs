ac1bb796bd1337ea4756982d984db50279ab882a
Drake Lin <drakelin21@hotmail.com>
2020-01-21 14:47:10 +0800

    +Person.cs

---
+++ SSIMS/Models/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SSIMS.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string FaxNumber { get; set; }
+        public string Email { get; set; }
+
+        public Person()
+        {
+        }
+
+        public Person(string Name, string PhoneNumber, string FaxNumber, string Email)
+        {
+            name = Name;
+            phoneNumber = PhoneNumber;
+            faxNumber = FaxNumber;
+            email = Email;
+        }
+    }
+}
