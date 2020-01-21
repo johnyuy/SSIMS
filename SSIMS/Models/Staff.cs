7e68187027a0c54f4c01bb36cc6fc823402897c1
Johann Fong <Johannfza@gmail.com>
2020-01-21 16:30:32 +0800

    Staff Change

--- SSIMS/Models/Staff.cs
+++ SSIMS/Models/Staff.cs
@@ -7,21 +7,25 @@ namespace SSIMS.Models
 {
     public class Staff: Person
     {
-        public string StaffID { get; set; }
+        public int StaffID { get; set; }
+        public int DepartmentID { get; set; }
+        public int UserAccountID { get; set; }
+        public string StaffRole { get; set; }
+
         public virtual Department Department { get; set; }
         public virtual UserAccount UserAccount { get; set; }
-        public string StaffRole { get; set; }
 
         public Staff()
         {
         }
 
-        public Staff(string StaffID, Department Department, UserAccount UserAccount, string StaffRole)
+        public Staff(string name, string phoneNumber, string faxNumber, 
+            string email, int departmentID, int userAccountID, string staffRole)     
+                    : base(name, phoneNumber, faxNumber, email)
         {
-            staffID = StaffID;
-            department = Department;
-            userAccount = UserAccount;
-            staffRole = StaffRole;
+            DepartmentID = departmentID;
+            UserAccountID = userAccountID;
+            StaffRole = staffRole;
         }
     }
 }
