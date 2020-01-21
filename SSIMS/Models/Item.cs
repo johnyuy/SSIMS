b62d814435d7b2bcf934a47f908739eca21c397c
johnyuy <57468127+johnyuy@users.noreply.github.com>
2020-01-21 14:59:57 +0800

    +person.cs

---
+++ SSIMS/Models/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SSIMS.Models
+{
+    public class Item
+    {
+        public string ItemID { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string UnitOfMeasure { get; set; }
+
+        public Item()
+        {
+        }
+
+        public Item(string itemID, string category, string description, string unitOfMeasure)
+        {
+            ItemID = itemID;
+            Category = category;
+            Description = description;
+            UnitOfMeasure = unitOfMeasure;
+        }
+    }
+}
