d97688a20f34021b5e90f71a2e5840eaaf99c12f
johnyuy <57468127+johnyuy@users.noreply.github.com>
2020-01-21 22:51:36 +0800

    CollectionPoint table initialized

--- SSIMS/Database/DatabaseInitializer.cs
+++ SSIMS/Database/DatabaseInitializer.cs
@@ -6,7 +6,7 @@ using SSIMS.Models;
 
 namespace SSIMS.Database
 {
-    public class DatabaseInitializer<T> : CreateDatabaseIfNotExists<DatabaseContext>
+    public class DatabaseInitializer<T> : DropCreateDatabaseAlways<DatabaseContext>
     {
         protected override void Seed(DatabaseContext context)
         {
@@ -14,6 +14,8 @@ namespace SSIMS.Database
             //Seed data
             InitCollectionPoints(context);
             context.SaveChanges();
+            //other initializations copy:    static void Init (DatabaseContext context)
+
             base.Seed(context);
         }
 
@@ -21,9 +23,13 @@ namespace SSIMS.Database
         {
             List<CollectionPoint> collectionPoints = new List<CollectionPoint>
             {
-                new CollectionPoint("Stationery Store", DateTime.Parse("9:30 AM"))
+                new CollectionPoint("Stationery Store", DateTime.Parse("9:30 AM")),
+                new CollectionPoint("Management School", DateTime.Parse("11:00 AM")),
+                new CollectionPoint("Medical School", DateTime.Parse("9:30 AM")),
+                new CollectionPoint("Engineering School", DateTime.Parse("11:00 AM")),
+                new CollectionPoint("Science School", DateTime.Parse("9:30 AM")),
+                new CollectionPoint("University Hospital", DateTime.Parse("11:00 AM"))
             };
-            Console.WriteLine(DateTime.Now.ToString("T"));
             foreach (CollectionPoint cp in collectionPoints)
                 context.CollectionPoints.Add(cp);
         }
