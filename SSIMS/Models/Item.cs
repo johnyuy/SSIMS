984eb0439ccec21d2bd6ab551e36d1e764cb65e8
johnyuy <57468127+johnyuy@users.noreply.github.com>
2020-01-21 10:46:53 +0800

    Completed Item class

--- SSIMS/Models/Item.cs
+++ SSIMS/Models/Item.cs
@@ -8,7 +8,23 @@ namespace SSIMS.Models
     public class Item
     {
         public string ItemID { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string UnitOfMeasure { get; set; }
 
+        public Item()
+        {
+        }
+
+        public Item(string itemID, string category, string description, string unitOfMeasure)
+        {
+            ItemID = itemID;
+            Category = category;
+            Description = description;
+            UnitOfMeasure = unitOfMeasure;
+        }
+
+        
 
     }
 }
