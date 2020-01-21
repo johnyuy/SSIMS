ca652694ae45ec870b888c864f1e1e7fabe285ee
Johann Fong <Johannfza@gmail.com>
2020-01-21 14:12:41 +0800

    Commit

---
+++ SSIMS/Models/Document.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace SSIMS.Models
+{
+
+    public class Document
+    {
+        public int DocumentID { get; set; }
+        public int CreatorID { get; set; }
+        public int ResponderID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ResponseDate { get; set; }
+
+        [ForeignKey("CreatorID")]
+        public Staff Creator { get; set; }
+
+        [ForeignKey("ResponderID")]
+        public Staff Responder { get; set; }
+    }
+}
