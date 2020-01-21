6e3cec4e7370f3ab45a423fd85615aad61f23d59
Johann Fong <Johannfza@gmail.com>
2020-01-21 13:25:13 +0800

    Commit

---
+++ SSIMS/fonts/Models/Document.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SSIMS.Models
+{
+    public enum Status
+    {
+        Pending, Approved, Rejected, Cancelled, Complete
+    }
+
+    public abstract class Document
+    {
+        public string DocumentID { get; set; }
+        public virtual Staff Creator { get; set; }
+        public virtual Staff Responder { get; set; }
+        public DateTime CreateDate{ get; set; }
+        public DateTime reponseDate { get; set; }
+        public Status Status { get; set; }
+
+    }
+}
---
+++ SSIMS/fonts/Models/RequisitionForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SSIMS.Models
+{
+    public class RequisitionForm : Document
+    {
+        public Dictionary<Item, int> RequestItemsMap { get; set; }
+        public string Comments { get; set; }
+
+        public RequisitionForm() 
+        {
+
+
+        }
+
+        public RequisitionForm()
+        {
+
+        }
+
+    }
+}
