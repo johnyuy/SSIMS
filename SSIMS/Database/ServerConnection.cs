e45cd4d838b673b41fde80e1e1f317a7baa4282d
Johann Fong <Johannfza@gmail.com>
2020-01-21 10:29:15 +0800

    Changed Data to SeverConnection

---
+++ SSIMS/Database/ServerConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SSIMS.Database
+{
+    public class ServerConnection
+    {
+        public static string ServerName = "JOHANNFONGA42F\SQLEXPRESS";
+        public static string DbName = "SSIMS";
+        public static string ConnectionString = "Server=" + ServerName + ";Database=" + DbName + ";Integrated Security=True";
+    }
+}
