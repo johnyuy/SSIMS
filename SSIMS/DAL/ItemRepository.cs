2286dad83763a81845c631722a489ff3a8158a92
Johann Fong <Johannfza@gmail.com>
2020-01-22 11:40:39 +0800

    Created Controller for Item Class

---
+++ SSIMS/DAL/ItemRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SSIMS.Models;
+using SSIMS.Database;
+
+namespace SSIMS.DAL
+{
+    public class ItemRepository : GenericRepository<Item>
+    {
+        public ItemRepository(DatabaseContext context)
+            : base(context)
+        {
+        }
+
+        public int UpdateItemFullStock(int id)
+        {
+            Console.WriteLine("UpdateItemFullStock");
+            return 1;
+            //return context.Database.ExecuteSqlCommand("UPDATE Course SET Credits = Credits * {0}", multiplier);
+        }
+
+    }
+}
