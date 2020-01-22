99ff0998254ffe469a921bbba5195053128a47e2
johnyuy <57468127+johnyuy@users.noreply.github.com>
2020-01-23 01:34:58 +0800

    Created SetDefaultID method for initializing entities

--- SSIMS/Database/DatabaseCustomizer.cs
+++ SSIMS/Database/DatabaseCustomizer.cs
@@ -10,28 +10,59 @@ namespace SSIMS.Database
 {
     public static class DatabaseCustomizer
     {
-        public static void SetDefaultID(DatabaseContext context)
+        public static void SetDefaultID<T>(T entityObj, string ID, int DefaultValue, DatabaseContext context)
         {
+            string table = (entityObj.GetType().Name) + "s";
+            string sql = "INSERT INTO " + table + " (";
+            List<string> attibuteList = new List<string>();
+            List<string> valueList = new List<string>();
 
+            foreach (var prop in entityObj.GetType().GetProperties())
+            {
+                if (prop.GetValue(entityObj, null) != null)
+                {
+                    if (prop.Name == ID)
+                    {
+                        sql += prop.Name;
+                    }
+                    else if (prop.GetValue(entityObj, null).ToString() != null)
+                    {
+                        attibuteList.Add(prop.Name);
+                        valueList.Add(prop.GetValue(entityObj, null).ToString());
+                    }
+                }
+            }
+            
+            for(int i = 0; i < attibuteList.Count; i++)
+            {
+                if (i < attibuteList.Count)
+                    sql += ", ";
+                sql += attibuteList[i];
+            }
+            sql += ") VALUES (" + DefaultValue.ToString();
+            for(int i = 0; i < valueList.Count; i++)
+            {
+                if (i < valueList.Count)
+                    sql += ", ";
+                sql += "'" + valueList[i]+"'";
+            }
+            sql += ")";
+            InsertCommand(sql, table, context);
+        }
+
+        static void InsertCommand(string sql, string table, DatabaseContext context)
+        {
             context.Database.Connection.Open();
             try
             {
-                context.Database.ExecuteSqlCommand("SET IDENTITY_INSERT dbo.Staffs ON");
-                context.Database.ExecuteSqlCommand("INSERT INTO STAFFS (StaffID, Name, DepartmentID) VALUES (100001,'John Yu',101)");
-                context.SaveChanges();
-                context.Database.ExecuteSqlCommand("SET IDENTITY_INSERT dbo.Staffs OFF");
+                context.Database.ExecuteSqlCommand("SET IDENTITY_INSERT dbo." + table + " ON");
+                context.Database.ExecuteSqlCommand(sql);
+                context.Database.ExecuteSqlCommand("SET IDENTITY_INSERT dbo." + table + " OFF");
             }
             finally
             {
                 context.Database.Connection.Close();
             }
-
-
-            foreach (var staff in context.Staffs)
-            {
-                Debug.WriteLine(staff.StaffID + ": " + staff.Name);
-            }
-            
         }
     }
 }
