5ff656276b02aaf4d404b5fbba763eae57da3dcd
Johann Fong <Johannfza@gmail.com>
2020-01-22 11:49:19 +0800

    ServerConnection

--- SSIMS/Database/ServerConnection.cs
+++ SSIMS/Database/ServerConnection.cs
@@ -7,42 +7,8 @@ namespace SSIMS.Database
 {
     public class ServerConnection
     {
-        public static string ServerName = "DRAKELIND20E";
+        public static string ServerName = "JOHANNFONGA42F\\SQLEXPRESS";
         public static string DbName = "SSIMS";
         public static string ConnectionString = "Server=" + ServerName + ";Database=" + DbName + ";Integrated Security=True";
     }
 }
-
-new Item("P037","Pen","Pen Transparency Soluble","Packet"),
-new Item("P038","Pen","Pen Whiteboard Marker Black","Box"),
-new Item("P039","Pen","Pen Whiteboard Marker Blue","Box"),
-new Item("P040","Pen","Pen Whiteboard Marker Green","Box"),
-new Item("P041","Pen","Pen Whiteboard Marker Red","Box"),
-new Item("P042","Pen","Pencil 2B","Dozen"),
-new Item("P043","Pen","Pencil 2B With Eraser End","Dozen"),
-new Item("P044","Pen","Pencil 4H","Dozen"),
-new Item("P045","Pen","Pencil B","Dozen"),
-new Item("P046","Pen","Pencil B With Eraser End","Dozen"),
-new Item("R001","Ruler","Ruler 6\"","Dozen"),
-new Item("R002","Ruler","Ruler 12\"","Dozen"),
-new Item("S100","Scissors","Scissors","Each"),
-new Item("S040","Tape","Scotch Tape","Each"),
-new Item("S041","Tape","Scotch Tape Dispenser","Each"),
-new Item("S101","Sharpener","Sharpener","Each"),
-new Item("S010","Shorthand","Shorthand Book (100 pg)","Each"),
-new Item("S011","Shorthand","Shorthand Book (120 pg)","Each"),
-new Item("S012","Shorthand","Shorthand Book (80 pg)","Each"),
-new Item("S020","Stapler","Stapler No.28","Each"),
-new Item("S021","Stapler","Stapler No.36","Each"),
-new Item("S022","Stapler","Stapler No.28","Box"),
-new Item("S023","Stapler","Stapler No.36","Box"),
-new Item("T001","Tacks","Thumb Tacks Large","Box"),
-new Item("T002","Tacks","Thumb Tacks Medium","Box"),
-new Item("T003","Tacks","Thumb Tacks Small","Box"),
-new Item("T020","Tparency","Transparency Blue","Box"),
-new Item("T021","Tparency","Transparency Clear","Box"),
-new Item("T022","Tparency","Transparency Green","Box"),
-new Item("T023","Tparency","Transparency Red","Box"),
-new Item("T024","Tparency","Transparency Reverse Blue","Box"),
-new Item("T025","Tparency","Transparency Cover 3M","Box"),
-new Item("T100","Tray","Trays In/Out","Box"),
