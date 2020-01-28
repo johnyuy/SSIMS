7be1146af9fa1b782a5579c265ebcc2a6c88df3b
Si Min Natalie Hong <e0478248@u.nus.edu>
2020-01-28 22:03:00 +0800

    updated DisbursementController

--- SSIMS/Controllers/DisbursementController.cs
+++ SSIMS/Controllers/DisbursementController.cs
@@ -1,13 +1,25 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using SSIMS.Models;
 
 namespace SSIMS.Controllers
 {
-    public class DisbursementController
+    public class DisbursementController : Controller
     {
+        public ICollection<TransactionItem> GenerateRetrievalList(RequisitionOrder requisition)
+        {
+            ArrayList retrievalList = new ArrayList();
+            ArrayList requisitionArray = new ArrayList(requisition.DocumentItems.ToArray());
+            foreach(DocumentItem doc in requisitionArray)
+            {
 
+            }
+            return null;
+        }
 
     }
 }
