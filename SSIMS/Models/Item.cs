1cf7aaefc03a1e576aef4934e4574a974b4ba311
johnyuy <57468127+johnyuy@users.noreply.github.com>
2020-01-30 10:10:38 +0800

    Inventory Controller, Layout with Logic University Icon

--- SSIMS/Models/Item.cs
+++ SSIMS/Models/Item.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,13 @@ namespace SSIMS.Models
     public class Item
     {
         [Key]
+        [DisplayName("Item Code")]
         public string ID { get; set; }
+        [DisplayName("Category")]
         public string Category { get; set; }
+        [DisplayName("Description")]
         public string Description { get; set; }
+        [DisplayName("Unit of Measure")]
         public string UnitOfMeasure { get; set; }
 
         public Item()
