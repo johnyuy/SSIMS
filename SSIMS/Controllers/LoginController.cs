0eecd25beb45771336f5937035eb07fecc6d39c4
johnyuy <57468127+johnyuy@users.noreply.github.com>
2020-02-01 10:16:15 +0800

    Login Controller, View and Service

--- SSIMS/Controllers/LoginController.cs
+++ SSIMS/Controllers/LoginController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SSIMS.Service;
+using SSIMS.ViewModels;
 
 namespace SSIMS.Controllers
 {
     public class LoginController : Controller
     {
+        
         // GET: Login
         public ActionResult Authenthication()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Authenticate(UserLogin userLogin)
+        {
+            ILoginService LoginService = new LoginService();
+            LoginService.VerifyPassword("ssimstestaccount", "password33");
+            
+            return RedirectToAction("Authenthication", userLogin);
         }
     }
 }
---
+++ SSIMS/Service/LoginService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SSIMS.DAL;
+using System.Diagnostics;
+
+namespace SSIMS.Service
+{
+    public class LoginService : ILoginService
+    {
+        UnitOfWork uow = new UnitOfWork();
+        public bool VerifyPassword(string username, string password)
+        {
+            Debug.WriteLine("Verifying Login username and password");
+            Debug.WriteLine("Username = " + username);
+            Debug.WriteLine("Password = " + password);
+
+            return false;
+        }
+    }
+}
