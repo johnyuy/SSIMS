da3b09a37c83957af5ff5cd50ef11f5a48fbf7b2
Si Min Natalie Hong <e0478248@u.nus.edu>
2020-02-01 14:20:19 +0800

    Created DeptRetrievalItemViewModel and RetrievalItemViewModel.

---
+++ SSIMS/ViewModels/DeptRetrievalItemViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SSIMS.Models;
+
+namespace SSIMS.ViewModels
+{
+    public class DeptRetrievalItemViewModel
+    {
+        public string deptID { get; set; }
+        public TransactionItem transactionItem { get; set; }
+
+
+        public DeptRetrievalItemViewModel(string deptID, TransactionItem transactionItem)
+        {
+            this.deptID = deptID;
+            this.transactionItem = transactionItem;
+        }
+    }
+}
