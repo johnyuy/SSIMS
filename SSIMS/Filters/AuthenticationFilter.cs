bf60b7e129ba15b6867d406bdfcb2be8e056b7ca
johnyuy <57468127+johnyuy@users.noreply.github.com>
2020-02-01 22:13:28 +0800

    Authentication Filter + Shared Layout Changes

--- SSIMS/Filters/AuthenticationFilter.cs
+++ SSIMS/Filters/AuthenticationFilter.cs
@@ -5,24 +5,45 @@ using System.Web;
 using System.Web.Mvc.Filters;
 using System.Diagnostics;
 using System.Web.Mvc;
+using SSIMS.Service;
 
 namespace SSIMS.Filters
 {
     public class AuthenticationFilter : ActionFilterAttribute, IAuthenticationFilter
     {
-        private bool _auth;
+        private bool authenticated = false;
         public void OnAuthentication(AuthenticationContext filterContext)
         {
-            
+            ILoginService loginService = new LoginService();
             string sessionid = HttpContext.Current.Session.SessionID;
-            string username = HttpContext.Current.Session[0].ToString();
-            Debug.WriteLine("On Authentication : " + username + " / " + sessionid);
-            _auth = true;
+            int c = HttpContext.Current.Session.Count;
+            int d = HttpContext.Current.Session.Keys.Count;
+            Debug.WriteLine("Session count = " + c);
+            Debug.WriteLine("Session count = " + d);
+
+            if(c > 0)
+            {
+                string username = HttpContext.Current.Session[0].ToString();
+                Debug.WriteLine("On Authentication : " + username + " / " + sessionid);
+                if (loginService.AuthenticateSession(username, sessionid))
+                {
+                    authenticated = true;
+                }
+            }
         }
 
         public void OnAuthenticationChallenge(AuthenticationChallengeContext filterContext)
         {
-            //throw new NotImplementedException();
+            if (!authenticated)
+            {
+                Debug.WriteLine("Authentication Failed!");
+                filterContext.Result = new RedirectToRouteResult(
+                        new System.Web.Routing.RouteValueDictionary
+                        {
+                            {"controller", "Login" },
+                            {"action","Authentication" }
+                        });
+            }
         }
 
        
