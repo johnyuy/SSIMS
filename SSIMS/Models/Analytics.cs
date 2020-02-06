5aa020e4aa829cf213dac0607321dcd0ede27823
Shuyuan Zhang <e0457795@u.nus.edu>
2020-02-06 10:15:36 +0800

    add analytics class

---
+++ SSIMS/Models/Analytics.cs
@@ -0,0 +1,23 @@
+﻿using SSIMS.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SSIMS.Models
+{
+    public class Analytics
+    {
+
+        public IEnumerable<RequisitionSummaryViewModel> categorylist { get;  set; }
+        public IEnumerable<RequisitionSummaryViewModel> categorylist2 { get;  set; }
+        public Analytics(List<RequisitionSummaryViewModel> categorylist, List<RequisitionSummaryViewModel> categorylist2)
+        {
+           
+            this.categorylist = categorylist;
+            this.categorylist2 = categorylist2;
+        }
+
+        
+    }
+}
