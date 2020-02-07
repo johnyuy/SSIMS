ec8543a788af3d1ac21f0e27860ee7a1c65be3b6
Gail0420 <58416019+Gail0420@users.noreply.github.com>
2020-02-07 21:58:02 +0800

    send Email & format for requisition

--- SSIMS/Controllers/RequisitionController.cs
+++ SSIMS/Controllers/RequisitionController.cs
@@ -12,7 +12,7 @@ using System.Web;
 using System.Web.Mvc;
 using Status = SSIMS.Models.Status;
 using PagedList;
-
+using System.Net.Mail;
 
 namespace SSIMS.Controllers
 {
@@ -668,8 +668,51 @@ namespace SSIMS.Controllers
         }
 
 
-        
 
+        public ActionResult SendEmail()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult SendEmail(string receiver, string subject, string message)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var senderEmail = new MailAddress("logicssims@outlook.com", "Logic University SSIMS");
+                    var receiverEmail = new MailAddress(receiver, "Receiver");
+                    var password = "ss1msadm1np@sswOrd";
+                    var sub = subject;
+                    var body = message;
+                    var smtp = new SmtpClient
+                    {
+                        Host = "smtp.outlook.com",
+                        Port = 587,
+                        EnableSsl = true,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        UseDefaultCredentials = false,
+                        Credentials = new NetworkCredential(senderEmail.Address, password)
+                    };
+                    using (var mess = new MailMessage(senderEmail, receiverEmail)
+                    {
+                        Subject = subject,
+                        Body = body
+                    })
+                    {
+                        smtp.Send(mess);
+                    }
+                    return View();
+                }
+            }
+            catch (Exception)
+            {
+                ViewBag.Error = "Some Error";
+            }
+            return View();
+        }
 
 
 
