a2751e8832baae94cedf37a1483f325b8768158f
Gail0420 <58416019+Gail0420@users.noreply.github.com>
2020-02-12 11:47:35 +0800

    merge

---
+++ SSIMS/ViewModels/ApiDocumentItem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SSIMS.Models;
+
+namespace SSIMS.ViewModels
+{
+    public class ApiDocumentItem
+    {
+        public String ID;
+        public String ItemID;
+        public String ItemDesc;
+        public String Qty;
+
+
+        public ApiDocumentItem(string iD, string itemID, string itemDesc, string qty)
+        {
+            ID = iD;
+            ItemID = itemID;
+            ItemDesc = itemDesc;
+            Qty = qty;
+        }
+        public ApiDocumentItem(DocumentItem di)
+        {
+            ID = di.ID.ToString();
+            ItemID = di.Item.ID.ToString();
+            ItemDesc = di.Item.Description.ToString();
+            Qty = di.Qty.ToString();
+        }
+
+        public ApiDocumentItem()
+        {
+        }
+    }
+
+
+
+}
---
+++ SSIMS/ViewModels/ApiPurchaseItemView.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SSIMS.Models;
+
+namespace SSIMS.ViewModels
+{
+    public class ApiPurchaseItemView
+    {
+        public string ID;
+        public string ItemID;
+        public string ItemDesc;
+        public string Qty;
+        public string UnitPrice;
+        public string Subtotal;
+
+
+        public ApiPurchaseItemView()
+        {
+        }
+
+        public ApiPurchaseItemView(string iD, string itemID, string itemDesc, string qty, string unitPrice, string subtotal)
+        {
+            ID = iD;
+            ItemID = itemID;
+            ItemDesc = itemDesc;
+            Qty = qty;
+            UnitPrice = unitPrice;
+            Subtotal = subtotal;
+        }
+
+        public ApiPurchaseItemView(PurchaseItem pi)
+        {
+            ID = pi.ID.ToString();
+            ItemID = pi.Tender.Item.ID;
+            ItemDesc = pi.Tender.Item.Description;
+            Qty = pi.Qty.ToString();
+            UnitPrice = pi.Tender.Price.ToString();
+            Subtotal = (pi.Tender.Price*pi.Qty).ToString();
+        }
+
+
+    }
+}
